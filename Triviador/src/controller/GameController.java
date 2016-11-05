00004578b4c5bb960314f4a5a98bd4189a44a299
Esteban Kramer <npaganin@itba.edu.ar>
2016-11-05 17:10:34 -0300

    Cree el package controller

---
+++ Triviador/src/controller/GameController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import java.io.Serializable;
+import model.*;
+import view.*;
+
+public class GameController implements Serializable {
+	private GameBoard view;
+	private Board model;
+}
