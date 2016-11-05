733454b6372a45f85a2b5ba32edc9cdc2ae080a4
Esteban Kramer <npaganin@itba.edu.ar>
2016-11-05 17:34:40 -0300

    Indente GameBoard para claridad y cambie cosas de Controller

--- Triviador/src/controller/GameController.java
+++ Triviador/src/controller/GameController.java
@@ -5,6 +5,15 @@ import model.*;
 import view.*;
 
 public class GameController implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	private GameBoard view;
 	private Board model;
+	
+	public GameController(GameBoard view, Board model) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	
 }
