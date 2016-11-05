8b451d00c293fa938513fe9a3871fc8510e7e5a3
Esteban Kramer <ekramer@itba.edu.ar>
2016-11-05 19:29:22 -0300

    Por Compatiblidad

--- Triviador/src/view/GameBoard.java
+++ Triviador/src/view/GameBoard.java
@@ -1,7 +1,5 @@
 package view;
 
-import java.io.Serializable;
-
 import java.awt.BorderLayout;
 
 import javax.swing.JButton;
@@ -11,89 +9,173 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
+import java.io.Serializable;
 import java.awt.event.ActionEvent;
 import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.Font;
+import javax.swing.JComboBox;
 
-public class GameBoard implements Serializable {
-
+public class GameBoard extends JFrame implements Serializable, ActionListener{
+	
 	private static final long serialVersionUID = 1L;
-
-	public GameBoard() {
+	private final JLabel attackFromLabel = new JLabel("Atacar desde:");
+	private final JLabel attackLabel = new JLabel("Atacar a:");
+	private final JComboBox attackFromComboBox = new JComboBox();
+	private final JComboBox attackComboBox = new JComboBox();
+	private final JTextField currentPlayerField = new JTextField();
+	private final JLabel playerOneLabel = new JLabel("Player 1");
+	private final JLabel playerTwoLabel = new JLabel("Player 2");
+	private final JLabel playerThreeLabel = new JLabel("Player 3");
+	
+	public GameBoard(){
 		final JLabel backgroundImage = new JLabel("");
-		final JButton btnNextTurn = new JButton("Pasar Turno");
+		final JButton NextTurnButton = new JButton("Pasar Turno");
 		final JLayeredPane layeredPane = new JLayeredPane();
-		final JTextField PlayerOneName = new JTextField();
-		final JTextField playerOneArmy = new JTextField();
-		final JLabel lblTurn = new JLabel("Turno:");
+		final JTextField playerOneArmyField = new JTextField();
+		final JLabel turnLabel = new JLabel("Turno:");
 		final JTextField turnNumber = new JTextField();
-		final JTextField PlayerTwoName = new JTextField();
-		final JTextField playerTwoArmy = new JTextField();
-		final JTextField PlayerThreeName = new JTextField();
-		final JTextField playerThreeArmy = new JTextField();
-		final JButton btnNewButton = new JButton("Atacar");
+		final JTextField playerTwoArmyField = new JTextField();
+		final JTextField playerThreeArmyField = new JTextField();
+		final JButton attackButton = new JButton("Atacar");
 		JFrame f = new JFrame("A JFrame");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setTitle("Triviador");
-		f.setSize(800, 600);
-		f.setLocation(300,200);
-		
-		JCharacter character = new JCharacter();
-		character.setVisible(true);
-    
-		f.getContentPane().add(layeredPane, BorderLayout.CENTER);
-		turnNumber.setBounds(509, 546, 103, 26);
-		layeredPane.add(turnNumber);
-		turnNumber.setColumns(10);
-		lblTurn.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
-		lblTurn.setBounds(509, 520, 103, 16);
-		layeredPane.add(lblTurn);
-		PlayerThreeName.setText("Player 3");
-		PlayerThreeName.setBounds(240, 515, 110, 26);
-		PlayerThreeName.setColumns(10);
-    
-		layeredPane.add(PlayerThreeName);
-		playerThreeArmy.setBounds(240, 546, 110, 26);
-		playerThreeArmy.setColumns(10);
-    
-		layeredPane.add(playerThreeArmy);
-		PlayerTwoName.setText("Player 2");
-		PlayerTwoName.setBounds(124, 515, 110, 26);
-		PlayerTwoName.setColumns(10);
-    
-		layeredPane.add(PlayerTwoName);
-		playerTwoArmy.setBounds(124, 546, 110, 26);
-		playerTwoArmy.setColumns(10);
-    
-		layeredPane.add(playerTwoArmy);
-		PlayerOneName.setBounds(8, 515, 110, 26);
-		layeredPane.add(PlayerOneName);
-		PlayerOneName.setText("Player 1");
-		PlayerOneName.setColumns(10);
-		playerOneArmy.setBounds(8, 546, 110, 26);
-		playerOneArmy.setColumns(10);
-    
-		layeredPane.add(playerOneArmy);
-		btnNewButton.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
-		btnNewButton.setSelectedIcon(new ImageIcon(GameBoard.class.getResource("resources/flag.png")));
-		btnNewButton.setBounds(218, 367, 71, 29);
-    
-		layeredPane.add(btnNewButton);
-		btnNextTurn.setBounds(612, 515, 182, 57);
-		layeredPane.add(btnNextTurn);
-		backgroundImage.setBounds(0, 0, 799, 509);
-		layeredPane.add(backgroundImage);
-		backgroundImage.setIcon(new ImageIcon(GameBoard.class.getResource("resources/map.jpg")));
-		btnNextTurn.addActionListener(new ActionListener() {
-    	
-			public void actionPerformed(ActionEvent e) {
-				return;
-			}
-		});
-		
-		JTextArea textArea = new JTextArea(10, 40);
-		textArea.setBackground(Color.BLACK);
-		f.setVisible(true);
-	}  	
+	    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    f.setTitle("Triviador");
+	    f.setSize(800, 600);
+	    f.setLocation(300,200);
+	    
+	    JCharacter character = new JCharacter();
+	    character.setVisible(true);
+	    
+	    f.getContentPane().add(layeredPane, BorderLayout.CENTER);
+	    turnNumber.setBounds(531, 543, 51, 26);
+	    layeredPane.add(turnNumber);
+	    turnNumber.setColumns(10);
+	    turnLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+	    turnLabel.setBounds(530, 524, 49, 16);
+	    layeredPane.add(turnLabel);
+	    playerOneLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+	    playerOneLabel.setBounds(8, 524, 51, 16);
+	    
+	    layeredPane.add(playerOneLabel);
+	    playerTwoLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+	    playerTwoLabel.setBounds(74, 524, 51, 16);
+	    
+	    layeredPane.add(playerTwoLabel);
+	    playerThreeLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+	    playerThreeLabel.setBounds(138, 524, 51, 16);
+	    
+	    layeredPane.add(playerThreeLabel);
+	    
+	    JButton btnAmericaCentral = new JButton("America Central");
+	    btnAmericaCentral.setBounds(138, 256, 117, 41);
+	    btnAmericaCentral.setBackground(Color.GREEN);
+	    layeredPane.add(btnAmericaCentral);
+	    
+	    JButton btnPoloNorte = new JButton("Polo Norte");
+	    btnPoloNorte.setBounds(226, 34, 117, 29);
+	    layeredPane.add(btnPoloNorte);
+	    
+	    JButton btnOceania = new JButton("Oceania");
+	    btnOceania.setBounds(613, 359, 117, 29);
+	    layeredPane.add(btnOceania);
+	    
+	    JButton btnEuropa = new JButton("Europa");
+	    btnEuropa.setBounds(368, 131, 117, 29);
+	    layeredPane.add(btnEuropa);
+	    
+	    JButton btnAsia_1 = new JButton("Medio Oriente");
+	    btnAsia_1.setBounds(508, 188, 117, 29);
+	    layeredPane.add(btnAsia_1);
+	    
+	    JButton btnAsia = new JButton("Asia");
+	    btnAsia.setBounds(573, 98, 117, 29);
+	    layeredPane.add(btnAsia);
+	    
+	    JButton btnAmericaDelNorte = new JButton("America del Norte");
+	    btnAmericaDelNorte.setBounds(88, 159, 136, 46);
+	    layeredPane.add(btnAmericaDelNorte);
+	    
+	    JButton btnNewButton = new JButton("Africa");
+	    btnNewButton.setBounds(368, 294, 117, 29);
+	    layeredPane.add(btnNewButton);
+	    
+	    JButton btnAmerica = new JButton("America del Sur");
+	    btnAmerica.setForeground(Color.BLACK);
+	    btnAmerica.setBackground(Color.CYAN);
+	    btnAmerica.setBounds(187, 346, 117, 54);
+	    layeredPane.add(btnAmerica);
+	    playerThreeArmyField.setBounds(138, 546, 60, 26);
+	    playerThreeArmyField.setColumns(10);
+	    
+	    layeredPane.add(playerThreeArmyField);
+	    playerTwoArmyField.setBounds(74, 546, 60, 26);
+	    playerTwoArmyField.setColumns(10);
+	    
+	    layeredPane.add(playerTwoArmyField);
+	    playerOneArmyField.setBounds(8, 546, 64, 26);
+	    playerOneArmyField.setColumns(10);
+	    
+	    layeredPane.add(playerOneArmyField);
+	    attackButton.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+	    attackButton.setSelectedIcon(new ImageIcon(GameBoard.class.getResource("resources/flag.png")));
+	    attackButton.setBounds(451, 519, 71, 54);
+	    
+	    layeredPane.add(attackButton);
+	    
+	    JButton saveGameButton = new JButton("Guardar");
+	    saveGameButton.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    	}
+	    });
+	    saveGameButton.setBounds(691, 518, 103, 57);
+	    layeredPane.add(saveGameButton);
+	    NextTurnButton.setBounds(584, 517, 103, 58);
+	    layeredPane.add(NextTurnButton);
+	    backgroundImage.setBounds(0, -4, 799, 484);
+	    layeredPane.add(backgroundImage);
+	    backgroundImage.setIcon(new ImageIcon(GameBoard.class.getResource("resources/map.jpg")));
+	    attackFromLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+	    attackFromLabel.setBounds(210, 519, 94, 16);
+	    
+	    layeredPane.add(attackFromLabel);
+	    attackLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+	    attackLabel.setBounds(334, 520, 103, 16);
+	    
+	    layeredPane.add(attackLabel);
+	    attackFromComboBox.setBounds(208, 544, 112, 27);
+	    attackFromComboBox.setSelectedIndex(0);
+	    attackFromComboBox.addActionListener(this);
+	    
+	    layeredPane.add(attackFromComboBox);
+	    attackComboBox.setBounds(330, 544, 114, 27);
+	    
+	    layeredPane.add(attackComboBox);
+	    
+	    JLabel lblEsElTurno = new JLabel("Es el turno del jugador:");
+	    lblEsElTurno.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+	    lblEsElTurno.setBounds(12, 492, 166, 16);
+	    layeredPane.add(lblEsElTurno);
+	    currentPlayerField.setColumns(10);
+	    currentPlayerField.setBounds(163, 487, 64, 26);
+	    
+	    
+	    
+	    layeredPane.add(currentPlayerField);
+	    NextTurnButton.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent e) {
+	    	}
+	    });
+	    JTextArea textArea = new JTextArea(10, 40);
+	    textArea.setBackground(Color.BLACK);
+	    f.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+			
+	}
+	
 }
