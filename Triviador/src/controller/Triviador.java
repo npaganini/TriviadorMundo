daddba9b5a65d440027d3a93878fee5cad244bf6
Clico <npaganin@itba.edu.ar>
2016-11-17 17:46:49 -0300

    Cambie getWinner.
    Devuelve null siempre excepto en el ultimo changeTurn. Ahora devuelve un
    Player en la ultima

--- Triviador/src/controller/Triviador.java
+++ Triviador/src/controller/Triviador.java
@@ -53,11 +53,11 @@ public class Triviador implements Serializable {
 	}
 	
 	public void addMultipleChoiceQuestions() {
-		multipleChoiceQuestions.add(new MultipleChoiceQuestion("¿En que deporte se usa tiza?", new String[] {"Futbol", "Tenis", "Golf", "Pool"}, 3 ));
-		multipleChoiceQuestions.add(new MultipleChoiceQuestion("¿Cuantas manos tiene un caballo?", new String[] {"0", "2", "3", "4"}, 1 ));
-		multipleChoiceQuestions.add(new MultipleChoiceQuestion("¿Cuales son las dos primeras palabras de la Biblia?", new String[] {"Una vez", "Al comienzo", "Al principio", "En ese"}, 2 ));
-		multipleChoiceQuestions.add(new MultipleChoiceQuestion("¿En que arbol crecen los datiles?", new String[] {"Palmera", "Sauce", "Tilo", "Ninguno"}, 0 ));
-		multipleChoiceQuestions.add(new MultipleChoiceQuestion("¿Cual es el segundo idioma mas hablado?", new String[] {"EspaÃ±ol", "Ingles", "Frances", "Chino"}, 1 ));
+		multipleChoiceQuestions.add(new MultipleChoiceQuestion("ï¿½En que deporte se usa tiza?", new String[] {"Futbol", "Tenis", "Golf", "Pool"}, 3 ));
+		multipleChoiceQuestions.add(new MultipleChoiceQuestion("ï¿½Cuantas manos tiene un caballo?", new String[] {"0", "2", "3", "4"}, 1 ));
+		multipleChoiceQuestions.add(new MultipleChoiceQuestion("ï¿½Cuales son las dos primeras palabras de la Biblia?", new String[] {"Una vez", "Al comienzo", "Al principio", "En ese"}, 2 ));
+		multipleChoiceQuestions.add(new MultipleChoiceQuestion("ï¿½En que arbol crecen los datiles?", new String[] {"Palmera", "Sauce", "Tilo", "Ninguno"}, 0 ));
+		multipleChoiceQuestions.add(new MultipleChoiceQuestion("ï¿½Cual es el segundo idioma mas hablado?", new String[] {"EspaÃ±ol", "Ingles", "Frances", "Chino"}, 1 ));
 	}
 	
 	public void addAproximationQuestions() {
@@ -102,6 +102,10 @@ public class Triviador implements Serializable {
         activePlayer = players.get(auxIndex);
     }
 	
+	/**
+	 * Checks amount of territories to assign a winner.
+	 * @returns Player who won.
+	 */
 	public Player getWinner() {
         Integer max = 0;
         for(Player p: players) {
