5d79b1990b0f2a822ee1dabf516e7da81c0c5dd5
Clico <npaganin@itba.edu.ar>
2016-11-17 18:02:39 -0300

    Agregue tests para controller

---
+++ Triviador/src/controller/TestJunitController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestJunitController {
+	static final int MAX_ROUNDS = 9;
+	
+	Triviador controllerTest = new Triviador();
+
+	//test to check appraisal
+	@Test
+	public void testCheckChngTurn() {
+		controllerTest.changeTurn();
+		int turnCount = controllerTest.getTurnCount() + 1;
+		int roundCount = controllerTest.getRoundCount() + 1;
+		assertEquals(turnCount, 2);
+		assertEquals(roundCount, 1);
+		
+		for(int i = 0; i < MAX_ROUNDS * 3; i++) {
+			controllerTest.changeTurn();
+		}
+		
+		//assertEquals()
+	}
+}
